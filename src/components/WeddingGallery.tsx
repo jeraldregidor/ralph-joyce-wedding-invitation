d62f126fba01b21ea0543686605de4e6347114b9
Jerald Regidor <jeraldregidor1997@gmail.com>
2026-03-28 00:49:06 +0800

    refactor photo lib

--- src/components/WeddingGallery.tsx
+++ src/components/WeddingGallery.tsx
@@ -1,60 +1,25 @@
 import { motion } from 'framer-motion';
-import { useMemo } from 'react';
 
-const images = [
-  './images/1.jpg',
-  './images/2.jpg',
-  './images/3.jpg',
-  './images/4.jpg',
-  './images/5.jpg',
-  './images/6.jpg',
-];
-
-const containerVariants = {
-  hidden: {},
-  visible: {
-    transition: {
-      staggerChildren: 0.15,
-    },
-  },
-};
-
-const itemVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
-};
 
 const WeddingGallery = () => {
-  const isMobile = useMemo(() => {
-    if (typeof window !== 'undefined') {
-      return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        window.navigator.userAgent
-      );
-    }
-    return false;
-  }, []);
-
-  const tapEffect = {
-    scale: 0.95,
-    transition: { type: 'spring', stiffness: 300, damping: 20 },
-  };
-
   return (
-    <section className="py-5 px-6 bg-white text-center border-b border-gray-100">
-      <motion.div
-        className="max-w-7xl mx-auto"
-        variants={containerVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.2 }}
-      >
-        <img
-            src="./images/PhotoLibrary.jpg"
-            alt="Invitation"
-            className="shadow-lg w-full max-w-7xl"
-          />
-      </motion.div>
-    </section>
+    <>
+      <section className="flex flex-col items-center justify-center py-6 px-6 bg-white text-center">
+        <motion.div
+          className="max-w-7xl text-center"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 0.6 }}
+        >
+          <img
+              src="./images/PhotoLibrary.jpg"
+              alt="Invitation"
+              className="shadow-lg w-full max-w-7xl"
+            />
+        </motion.div>
+      </section>
+    </>
   );
 };
 
